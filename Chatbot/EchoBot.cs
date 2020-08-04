e7927e620c4e1e8d88ddfa3a4a5078dac5059d6b
TheHong <chcheung.office@gmail.com>
2020-08-04 18:32:19 -0400

    Name the bot Felix

--- Chatbot/EchoBot.cs
+++ Chatbot/EchoBot.cs
@@ -1,4 +1,7 @@
 ﻿// Adapted from EchoBot .NET Template version v4.9.1
+/*
+Despite its name, it's not actually an echo bot. It is only based on the echo bot structure.
+*/
 
 using System.Collections.Generic;
 using System.Threading;
@@ -14,6 +17,7 @@ namespace MyEchoBot.Bots
 
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+
             var text = turnContext.Activity.Text;
             string info;
 
@@ -44,38 +48,42 @@ namespace MyEchoBot.Bots
             var replyText = $"You just typed: {text}. Info: {info}";
 
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome! My name is MHBOT. THIS IS AWESOME";
-            foreach (var member in membersAdded)
-            {
-                if (member.Id != turnContext.Activity.Recipient.Id)
-                {
-                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
-                }
-            }
+            await SendIntroCardAsync(turnContext, cancellationToken);
+            // var welcomeText = "Hello and welcome! My name is MHBOT. THIS IS AWESOME";
+            // foreach (var member in membersAdded)
+            // {
+            //     if (member.Id != turnContext.Activity.Recipient.Id)
+            //     {
+            //         await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+            //     }
+            // }
         }
 
-        // private static async Task SendIntroCardAsync(ITurnContext turnContext, CancellationToken cancellationToken)
-        // {
-        //     var card = new HeroCard();
-        //     card.Title = "Welcome to Bot Framework!";
-        //     card.Text = @"Welcome to Welcome Users bot sample! This Introduction card
-        //             is a great way to introduce your Bot to the user and suggest
-        //             some things to get them started. We use this opportunity to
-        //             recommend a few next steps for learning more creating and deploying bots.";
-        //     card.Images = new List<CardImage>() { new CardImage("https://aka.ms/bf-welcome-card-image") };
-        //     card.Buttons = new List<CardAction>()
-        //     {
-        //         new CardAction(ActionTypes.OpenUrl, "Get an overview", null, "Get an overview", "Get an overview", "https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0"),
-        //         new CardAction(ActionTypes.OpenUrl, "Ask a question", null, "Ask a question", "Ask a question", "https://stackoverflow.com/questions/tagged/botframework"),
-        //         new CardAction(ActionTypes.OpenUrl, "Learn how to deploy", null, "Learn how to deploy", "Learn how to deploy", "https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-deploy-azure?view=azure-bot-service-4.0"),
-        //     };
+        private static async Task SendIntroCardAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var card = new HeroCard();
+            card.Title = "Hi, my name is Felix!";
+            card.Text = "I am a chatbot designed to talk to you about mental " +
+                "health resources available at the University of Toronto. As a " +
+                "chatbot, I am not human. As a result, please note that my conversation " +
+                "skills might be limited, despite my responses being human-like. " +
+                "At any time you have the option to speak with a human operator. " +
+                "You may now type something are click on one of the following options to get started.";
+            card.Images = new List<CardImage>() { new CardImage("https://aka.ms/bf-welcome-card-image") };
+            card.Buttons = new List<CardAction>()
+            {
+                new CardAction(ActionTypes.OpenUrl, "Tell me about yourself, Felix.", null, "Get an overview", "Get an overview", "https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0"),
+                new CardAction(ActionTypes.OpenUrl, "Please redirect me to an operator.", null, "Ask a question", "Ask a question", "https://stackoverflow.com/questions/tagged/botframework"),
+                new CardAction(ActionTypes.OpenUrl, "How are you today, Felix?", null, "Learn how to deploy", "Learn how to deploy", "https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-deploy-azure?view=azure-bot-service-4.0"),
+            };
 
-        //     var response = MessageFactory.Attachment(card.ToAttachment());
-        //     await turnContext.SendActivityAsync(response, cancellationToken);
-        // }
+            var response = MessageFactory.Attachment(card.ToAttachment());
+            await turnContext.SendActivityAsync(response, cancellationToken);
+        }
     }
 }
