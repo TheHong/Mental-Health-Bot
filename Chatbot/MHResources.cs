ccbe55d16691a92c0f63081fb137d432f5fe30db
TheHong <chcheung.office@gmail.com>
2020-08-06 17:05:08 -0400

    Add ToStr function for Resource objects

--- Chatbot/MHResources.cs
+++ Chatbot/MHResources.cs
@@ -1,19 +1,32 @@
 namespace MyEchoBot
 {
-    public class Resource{
+    public class Resource
+    {
         public string title;
         public string link;
         public string info;
         public string[] tags;
+
+        public string ToStr()
+        {
+            string[] info = [
+                title.ToUpper() + "\n\n",
+                info + "\n",
+                $"You can find more info at {link}"
+            ];
+            return string.Join(info);
+        }
     }
-    
+
     public class InputProcessing
     {
-        public string[] GetTags(string[] keywords){
+        public string[] GetTags(string[] keywords)
+        {
             return new string[1];
         }
 
-        public Resource[] GetResources(string[] tags){
+        public Resource[] GetResources(string[] tags)
+        {
             return new Resource[1];
         }
     }
